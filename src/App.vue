1be86787964f4be8d8f43f8829bd000679d60708
awebr000 <mas.awe.awe@gmail.com>
2017-09-29 22:44:30 +0700

    initial init

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '../node_modules/bulma/css/bulma.css';
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+html, body {
+  overflow: auto;
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+.centered {
+  text-align: center !important;
+}
+
+.p-0 {
+  padding: 0px !important;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="login-page">
+    <!-- <div class="columns is-centered">
+      <div class="column is-half centered" style="padding: 64px 32px">
+        <p class="title">Welcome To Asia Commerce Admin!</p>
+      </div>
+    </div> -->
+    <div class="columns is-centered">
+      <div class="column is-one-quarter is-half-mobile is-offset-one-quarter-mobile is-two-thirds-tablet admin-login">
+        <div class="field centered">
+            <label for="" class="title"> Your Image will Be here</label>
+            <label class="label light-label">Admin Panel</label>
+        </div>
+        <div>
+          <div class="field">
+            <div class="control">
+              <input class="input" v-model="email" type="email" name="email" placeholder="Email">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <input class="input" v-model="password" type="password" placeholder="Password">
+            </div>
+          </div>
+          <div style="padding-top: 45px;" class="columns">
+              <button @click="submit" class="column button is-info p-0">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  #login-page {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    background: url('http://admin.qa.asiacommerce.net/assets/images/backgrounds/march.jpg'), no-repeat;
+    background-size: cover;
+  }
+  .admin-login {
+    position: absolute;
+    top: 25%;
+    background-color: white;
+    padding: 48px;
+  }
+</style>
